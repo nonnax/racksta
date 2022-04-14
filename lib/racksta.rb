13a78a2d93cd2e2c64cd844d580b5ce5ffec7735
nonnax <ironald@gmail.com>
2022-04-14 14:06:44 +0800

    refactor

--- lib/racksta.rb
+++ lib/racksta.rb
@@ -16,26 +16,24 @@ module Racksta
   class App 
     attr :path_file, :status, :res, :req
 
-    def get_path_file(env) 
+    def eval_path_file(env) 
       if env["REQUEST_METHOD"] == "GET"
         @path_file = ::Racksta.map[env["PATH_INFO"]]
         @status = @path_file.nil?? 404 : 200
       end
       @path_file ||= :not_found_md
-      [path_file, status] 
     end 
 
     def _call(env)
       @req=Rack::Request.new(env)
       @res=Rack::Response.new
       catch(:halt){
-        path_file, status = get_path_file(env)
+        eval_path_file(env)
         body = View.render(path_file, **req.params.transform_keys(&:to_sym))
-        
         [status, {Rack::CONTENT_TYPE=>'text/html; charset=utf-8'}, [body]]
       }
     rescue Exception => e
-      [404, {}, ["Not Found : " + e.message.partition('@').first]]    
+      [500, {}, ['Internal Error: ' + e.message.partition('@').first]]    
     end
     def call(env) dup._call(env) end
     def halt(res) throw :halt, res end
